089b04af4ba7fc04900885c2680fd3bfcf670c37
vadim <v.spiridonov.stc@innopolis.university>
2018-09-28 12:32:57 +0300

    Fixed Test Class

--- src/test/java/wordFinder/WordFinderUserTest.java
+++ src/test/java/wordFinder/WordFinderUserTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class fWordFinderUserTest {
+class WordFinderUserTest {
     public static final String FILE_ANY = "file://any";
     public static final String WORD = "";
     private WordFinderUser wordFinderUser;
